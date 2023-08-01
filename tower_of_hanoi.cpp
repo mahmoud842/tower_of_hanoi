3682f7abdd8cb2672d430f48b12e7e87297d4b7d
mahmoud842 <62242454+mahmoud842@users.noreply.github.com>
2023-08-01 21:09:56 +0300

    Update tower_of_hanoi.cpp add comments in it

--- tower_of_hanoi.cpp
+++ tower_of_hanoi.cpp
@@ -11,7 +11,7 @@
 
 int main() {
 
-	hanoiTower::Game g(5);
+	hanoiTower::Game g(5); // enter the number of discs in the game as an argument.
 	g.printGame();
 	g.solve();
 	std::cout << "========after solve========" << std::endl;
